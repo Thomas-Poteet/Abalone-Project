e0a7308f5e275e4d8c16a0277a4ae498af7efecc
DevanHoover <142552755+DevanHoover@users.noreply.github.com>
2024-11-13 20:49:22 -0600

    Added Packages/Loaded Packages
    Added code to install and load the packages tidyverse, Metrics, modelr, broom, and lmtest. Additionally, summarized the data for more information

--- Abalone Project/Abalone.R
+++ Abalone Project/Abalone.R
@@ -1,3 +1,16 @@
+install.packages(c("tidyverse", "Metrics", "modelr", "broom", "lmtest", "zoo", "sandwich", "rstatix"))
+
+library(tidyverse)
+library(Metrics)
+library(modelr)
+library(broom)
+library(lmtest)
+library(zoo)
+library(sandwich)
+library(rstatix)
+
 data = read.csv("abalone.data")
 colnames(data) <- c("Sex","Length", "Diameter", "Height", "Whole_Weight", "Schucked_Weight", "Viscera_Weight", "Shell Weight", "Rings")
 data
+
+summary(data)
