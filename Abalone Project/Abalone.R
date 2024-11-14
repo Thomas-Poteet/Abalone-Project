808ca516cb690caf860d73a235e700285f04a207
DevanHoover <142552755+DevanHoover@users.noreply.github.com>
2024-11-14 14:15:58 -0600

    Added ggplot2 to the packages

--- Abalone Project/Abalone.R
+++ Abalone Project/Abalone.R
@@ -1,4 +1,4 @@
-install.packages(c("tidyverse", "Metrics", "modelr", "broom", "lmtest", "zoo", "sandwich", "rstatix"))
+install.packages(c("tidyverse", "Metrics", "modelr", "broom", "lmtest", "zoo", "sandwich", "rstatix" , "ggplot2"))
 
 library(tidyverse)
 library(Metrics)
@@ -8,9 +8,11 @@ library(lmtest)
 library(zoo)
 library(sandwich)
 library(rstatix)
+library(ggplot2)
 
 data = read.csv("abalone.data")
 colnames(data) <- c("Sex","Length", "Diameter", "Height", "Whole_Weight", "Schucked_Weight", "Viscera_Weight", "Shell Weight", "Rings")
 data
 
 summary(data)
+
